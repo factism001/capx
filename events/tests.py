67db76e74837d759116892429c12869ca100f9f0
factism001 <factism001@gmail.com>
2023-10-21 13:56:17 +0100

    added testcase for the events app model

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Event
 
 # Create your tests here.
+
+class EventModelTest(TestCase):
+    def test_event_creation(self):
+        # Test creating an Event instance
+        event = Event.objects.create(
+            name="Test Event",
+            creator=None,
+            date_of_creation="2023-10-20",
+            date_of_last_edit="2023-10-20",
+            description="Test description",
+            wiki_link="http://example.com",
+            in_person_event=True,
+            country="Test Country",
+        )
+
+        self.assertEqual(event.name, "Test Event")
+        self.assertEqual(event.in_person_event, True)
